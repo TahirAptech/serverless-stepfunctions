c0596b8262420893bdc82f09e883301e82f2a3e4
mughees605 <muhammadmughees08@gmail.com>
2023-05-16 20:37:33 +0500

    sls-stepfunction

---
+++ functions/extractData.js
@@ -0,0 +1,7 @@
+exports.handler = async (event) => {
+    // Code to extract data from the database
+    console.log("first state's lambda event:", event);
+
+    return event;
+};
+
---
+++ functions/hello.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const stepFunctions = new AWS.StepFunctions();
+// AWS.config.update({ region: 'us-east-1' }); 
+
+exports.handler = async (event) => {
+    // ARN of state machine
+    const stateMachineArn = 'arn:aws:states:us-east-1:032751846634:stateMachine:MyStateMachineStepFunctionsStateMachine-O31dxd3TYwRd';
+
+    // Provide input data for the first state here
+    const input = {
+        data: "Extracted data from the database",
+        name: "Tahir Mahmood Hashmi",
+        address: "Shadman Town"
+    };
+
+    const params = {
+        stateMachineArn,
+        input: JSON.stringify(input),
+    };
+
+    try {
+        const result = await stepFunctions.startExecution(params).promise();
+        console.log('Step Functions execution started:', result);
+    } catch (error) {
+        console.error('Error starting Step Functions execution:', error);
+    }
+}
