ba73930cc98c649f47e8e2755506452b36331057
mughees605 <muhammadmughees08@gmail.com>
2023-05-23 14:43:34 +0500

    Changes

--- projects/Project1/functions/processData.js
+++ projects/Project1/functions/processData.js
@@ -1,15 +1,14 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
-    // const params = {
-    //     TableName: "ProductTable",
-    //     IndexName: 'ByOrgId',
-    //     KeyConditionExpression: "org_id = :orgId",
-    //     ExpressionAttributeValues: {
-    //         ":orgId": event
-    //     }
-    // };
-    // let { Items } = await dynamoDb.query(params).promise();
-    // return Items;
-    return event;
+    const params = {
+        TableName: event.item,
+        IndexName: 'ByOrgId',
+        KeyConditionExpression: "org_id = :orgId",
+        ExpressionAttributeValues: {
+            ":orgId": event.resources
+        }
+    };
+    let { Items } = await dynamoDb.query(params).promise();
+    //return {data: Items};
 };
